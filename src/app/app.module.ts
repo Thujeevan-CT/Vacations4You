41dde36959d9ca647a39cea1d63f44baa2396494
SarangaGamage <sarangag@emojot.com>
2023-11-10 22:33:48 +0530

    adding new nav bar  component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './features/vacation4u-user-web/common/nav-bar/nav-bar.component';
+
 // import { Vacation4uUserWebComponent } from './features/vacation4u-user-web/vacation4u-user-web/vacation4u-user-web.component';
 
 @NgModule({
--- src/app/features/vacation4u-user-web/vacation4u-user-web.module.ts
+++ src/app/features/vacation4u-user-web/vacation4u-user-web.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { Vacation4uUserWebRoutingModule } from './vacation4u-user-web-routing.module';
+import { NavBarComponent } from './common/nav-bar/nav-bar.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavBarComponent
+  ],
   imports: [
     CommonModule,
     Vacation4uUserWebRoutingModule
